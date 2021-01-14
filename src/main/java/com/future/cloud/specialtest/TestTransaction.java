e5ba0baa20e0562080515247ac2430e5d08faf62
zgy <1326750256@qq.com>
2021-01-14 18:24:30 +0800

    整合mybatis

---
+++ src/main/java/com/future/cloud/specialtest/TestTransaction.java
@@ -0,0 +1,7 @@
+package com.future.cloud.specialtest;
+
+public class TestTransaction {
+
+
+
+}
---
+++ src/main/java/com/future/cloud/testcontroller/TestTransactionController.java
@@ -0,0 +1,21 @@
+package com.future.cloud.testcontroller;
+
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestTransactionController {
+
+    @GetMapping("/testTransactionEventListener")
+    @TransactionalEventListener(fallbackExecution = true, phase = TransactionPhase.AFTER_COMMIT)
+    public void testTransactionEventListener() {
+
+
+
+    }
+
+}
