4ad8fb30c10d33c46d07552e6b54b6eaca8f50fb
zgy <1326750256@qq.com>
2021-01-14 13:18:10 +0800

    项目初始化

---
+++ src/test/java/com/future/cloud/CollectTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectTechnologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
