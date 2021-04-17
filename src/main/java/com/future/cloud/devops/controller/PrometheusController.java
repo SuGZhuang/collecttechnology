6017ca25807bce49e77d5df1cf9051434f0daedc
zgy <1326750256@qq.com>
2021-04-17 19:48:42 +0800

    PrometheusController

---
+++ src/main/java/com/future/cloud/devops/controller/PrometheusController.java
@@ -0,0 +1,24 @@
+package com.future.cloud.devops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author guangyuan.zhuang
+ * @date 2021年04月17日  19:26
+ */
+@Controller
+@RequestMapping("api")
+public class PrometheusController {
+
+    /**
+     * @description 重定向到Prometheus
+     */
+    public void redirectPrometheus(HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("https://www.zgy1314.cn/prometheus");
+    }
+
+}
