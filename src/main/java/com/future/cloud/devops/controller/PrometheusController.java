1b86f270cad662554303399804083ea1088eb0a0
zgy <1326750256@qq.com>
2021-04-17 21:19:03 +0800

    getmapping

--- src/main/java/com/future/cloud/devops/controller/PrometheusController.java
+++ src/main/java/com/future/cloud/devops/controller/PrometheusController.java
@@ -1,6 +1,7 @@
 package com.future.cloud.devops.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +12,13 @@ import java.io.IOException;
  * @date 2021年04月17日  19:26
  */
 @Controller
-@RequestMapping("api")
+@RequestMapping("/api")
 public class PrometheusController {
 
     /**
      * @description 重定向到Prometheus
      */
+    @GetMapping("/v1/redirectPrometheus")
     public void redirectPrometheus(HttpServletResponse resp) throws IOException {
         resp.sendRedirect("https://www.zgy1314.cn/prometheus");
     }
